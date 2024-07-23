c9b6b99221ffe79e5530c6f79dad38e7d46dda37
Valentin Afonso <valentinafonso22@gmail.com>
2024-07-23 22:11:30 +0200

    ⚡️setup dato cms to next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { performRequest } from "@/lib/datocms";
+const PAGE_CONTENT_QUERY = `
+  query Home {
+    homePage {
+      title
+      test {
+        ... on BlogSliderRecord {
+          id
+        }
+        ... on TextRecord {
+          id
+        }
+        ... on LinkRecord {
+          id
+          linkLabel
+          url
+        }
+      }
+    }
+  }`;
+export default async function Home() {
+  const {
+    data: { homePage },
+  } = await performRequest({ query: PAGE_CONTENT_QUERY });
+  console.log(homePage);
+
   return (
     <div>
-      <h1>Staytuneed</h1>
+      <h1>{homePage.title}</h1>
     </div>
   );
 }
