45ec5d416d27e6fc03f334745995161a84076107
Valentin Afonso <valentinafonso22@gmail.com>
2024-08-12 19:55:39 +0200

    🐛 fix issue undefined data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { queryHomePage } from "@/cms/queries/queryHomePage";
 
 export default async function Home() {
   const { homePage } = await performRequest({ query: queryHomePage });
-
+  if (!homePage) return <div>...</div>;
   return (
     <div>
       <h1>{homePage?.title}</h1>
