c850acf253fb496b3658ad6a4a2f92d9bb3d7c84
Valentin Afonso <valentinafonso22@gmail.com>
2024-09-08 22:48:44 +0200

    🐛fix author component issue

--- src/ui/Author.tsx
+++ src/ui/Author.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 export default function Author({ author }: any) {
+  if (!author) return <></>;
   return (
     <div className="flex flex-col items-center mb-16">
-      <p className="text-gray-light">written by</p>
+      <p className="text-gray-light pb-4">written by</p>
       <Image
         src={author.pictureProfil?.url}
         width={author.pictureProfil?.width}
