b5c5ea1be978083ea129a5fda38be7817e8c35f2
Valentin Afonso <valentinafonso22@gmail.com>
2024-09-15 21:34:32 +0200

    fix twotter card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,25 @@ import SectionFaq from "@/ui/SectionFaq";
 export async function generateMetadata() {
   return {
     robots: "index,follow",
+    openGraph: {
+      title: "Staytuneed",
+      description:
+        "Welcome to Staytuneed, your friendly hub for everything tech, design, and web development! Whether you're diving into React, Next.js, or JavaScript, or looking for the latest design tips and tech news, we've got you covered. Our easy-to-follow tutorials, expert advice, and creative inspiration are here to help developers and designers of all levels grow their skills and stay ahead in the ever-changing world of tech. Stay tuned for content that makes learning fun and keeps you in the know!",
+      images: [
+        {
+          url: "https://www.staytuneed.com/twitter-image.jpg",
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    twitter: {
+      title: "Staytuneed",
+      description:
+        "Welcome to Staytuneed, your friendly hub for everything tech, design, and web development! Whether you're diving into React, Next.js, or JavaScript, or looking for the latest design tips and tech news, we've got you covered. Our easy-to-follow tutorials, expert advice, and creative inspiration are here to help developers and designers of all levels grow their skills and stay ahead in the ever-changing world of tech. Stay tuned for content that makes learning fun and keeps you in the know!",
+      card: "summary_large_image",
+      image: "https://www.staytuneed.com/twitter-image.jpg",
+    },
   };
 }
 
