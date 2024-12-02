ea56b0fb57a11edf3bca3e7db6e6f566875afab4
Valentin Afonso <valentinafonso22@gmail.com>
2024-12-02 21:55:27 +0100

    ⚡️clean code

--- src/ui/Filter.tsx
+++ src/ui/Filter.tsx
@@ -8,7 +8,6 @@ type ButtonPrimaryType = {
   tag: any;
 };
 export default function Filter({ children, tag }: ButtonPrimaryType) {
-  // const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const router = useRouter();
   const searchParams = useSearchParams();
   const [selectedTags, setSelectedTags] = useState<string[]>(
@@ -16,23 +15,6 @@ export default function Filter({ children, tag }: ButtonPrimaryType) {
   );
 
   const handleTagToggle = (tagSlug: string) => {
-    /*
-    const currentTags = searchParams.get("tags")?.split(",") || [];
-    let updatedTags;
-    if (currentTags.includes(tagSlug)) {
-      updatedTags = currentTags.filter((t) => t !== tagSlug);
-    } else {
-      updatedTags = [...currentTags, tagSlug];
-    }
-
-    const params = new URLSearchParams(searchParams);
-    if (updatedTags.length > 0) {
-      params.set("tags", updatedTags.join(","));
-    } else {
-      params.delete("tags");
-    }
-    router.push(`/blog?${params.toString()}`);
-    */
     const updatedTags = selectedTags.includes(tagSlug)
       ? selectedTags.filter((t) => t !== tagSlug)
       : [...selectedTags, tagSlug];
