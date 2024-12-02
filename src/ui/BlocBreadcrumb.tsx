eff84e498a37e3cbb298ccb4cfd3ebd3672c87b2
Valentin Afonso <valentinafonso22@gmail.com>
2024-12-02 21:51:15 +0100

    ⚡️ optimize filternig

--- src/ui/BlocBreadcrumb.tsx
+++ src/ui/BlocBreadcrumb.tsx
@@ -6,6 +6,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Fragment } from "react";
 
 interface BreadcrumbProps {
   items: { url: string; title: string }[];
@@ -13,16 +14,16 @@ interface BreadcrumbProps {
 
 export default function BlocBreadcrumb({ items }: BreadcrumbProps) {
   const listItems = items.map((item, index) => (
-    <>
+    <Fragment key={index}>
       {index > 0 && <BreadcrumbSeparator>/</BreadcrumbSeparator>}
-      <BreadcrumbItem key={index}>
+      <BreadcrumbItem>
         {index === items.length - 1 ? (
           <BreadcrumbPage>{item.title}</BreadcrumbPage>
         ) : (
           <BreadcrumbLink href={item.url}>{item.title}</BreadcrumbLink>
         )}
       </BreadcrumbItem>
-    </>
+    </Fragment>
   ));
   return (
     <Breadcrumb className="mb-4">
--- src/ui/Filter.tsx
+++ src/ui/Filter.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 type ButtonPrimaryType = {
   children: React.ReactNode;
@@ -10,8 +11,12 @@ export default function Filter({ children, tag }: ButtonPrimaryType) {
   // const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [selectedTags, setSelectedTags] = useState<string[]>(
+    searchParams.get("tags")?.split(",") || []
+  );
 
   const handleTagToggle = (tagSlug: string) => {
+    /*
     const currentTags = searchParams.get("tags")?.split(",") || [];
     let updatedTags;
     if (currentTags.includes(tagSlug)) {
@@ -19,6 +24,20 @@ export default function Filter({ children, tag }: ButtonPrimaryType) {
     } else {
       updatedTags = [...currentTags, tagSlug];
     }
+
+    const params = new URLSearchParams(searchParams);
+    if (updatedTags.length > 0) {
+      params.set("tags", updatedTags.join(","));
+    } else {
+      params.delete("tags");
+    }
+    router.push(`/blog?${params.toString()}`);
+    */
+    const updatedTags = selectedTags.includes(tagSlug)
+      ? selectedTags.filter((t) => t !== tagSlug)
+      : [...selectedTags, tagSlug];
+
+    setSelectedTags(updatedTags);
 
     const params = new URLSearchParams(searchParams);
     if (updatedTags.length > 0) {
